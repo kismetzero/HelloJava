1afe6535ae8796efe94cc97f078a504c70d97b13
KismetPC-Win10 <1500632363@qq.com>
2022-08-22 21:53:17 +0800

    [22/08/22] 重构HelloSpring 测试使用XML配置文件，注解，Java-conf管理对象

---
+++ B_HelloJavaEE/G_HelloSpring/src/main/java/com/kismet/anno/Person.java
@@ -0,0 +1,60 @@
+package com.kismet.anno;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class Person {
+    @Value("李四")
+    private String name;
+    @Value("20")
+    private int age;
+
+    public Person() {
+        System.out.println("Person的无参构造被调用——注解创建");
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
